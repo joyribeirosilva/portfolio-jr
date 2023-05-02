80c7cbc337e2b256a213e92a401ba1f253392b01
joyribeirosilva <joyceribeirosilva1@gmail.com>
2023-05-02 00:14:04 -0300

    Portfólio

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
+import Header from '../../Components/Header'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Head>
+      <title>Joy | dev</title>
+    </Head>
+    <Header/>
+    <Component {...pageProps} />
+    </>
+  
+  
+  )
 }
